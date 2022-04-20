502657a980409c02df7b55f70ff4e6adbdabbd83
Andreea Hazu <andreeadhazu@gmail.com>
2022-04-20 02:34:09 +0200

    handle division by 0

--- TODO_detailed_evaluation.py
+++ TODO_detailed_evaluation.py
@@ -2,6 +2,12 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import classification_report
 
+# Ignore sklearn forcefull warnings
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
 def evaluate_predictions_detailed(df, pos, neg):
     df.columns = ['word', 'gold', 'prediction']
     cm = confusion_matrix(df, pos, neg)
@@ -11,13 +17,13 @@ def evaluate_predictions_detailed(df, pos, neg):
     return pr, rec, f1mes
 
 def precision(cm):
-    return round(cm[0,0]/(cm[0,0] + cm[1,0]),2)
+    return round(cm[0,0]/(cm[0,0] + cm[1,0]),2) if (cm[0,0] + cm[1,0] != 0) else 0
 
 def recall(cm):
     return round(cm[0,0]/(cm[0,0] + cm[0,1]),2)
 
 def f1(pr, rec):
-    return round(2*(pr * rec)/(pr + rec),2)
+    return round(2*(pr * rec)/(pr + rec),2) if (pr + rec != 0) else 0
 
 def confusion_matrix(df, pos, neg):
     P = np.sum(df.prediction == pos)
@@ -39,8 +45,8 @@ def find_metric(df):
     # for Class C
     pr_C, rec_C, f1mes_C = evaluate_predictions_detailed(df, 'C', 'N')
     # Weighted Average F1
-    nn = np.sum(df.prediction == 'N')
-    nc = np.sum(df.prediction == 'C')
+    nn = np.sum(df.gold == 'N')
+    nc = np.sum(df.gold == 'C')
     wa_f1 = round((f1mes_N * nn + f1mes_C * nc) / (nn + nc),2)
     
     return [pr_N, rec_N, f1mes_N, pr_C, rec_C, f1mes_C, wa_f1]
@@ -49,10 +55,16 @@ if __name__ == '__main__':
     """
         Gives detailed evaluation on predictions from evaluate.py
     """
-    df = pd.read_csv("experiments/base_model/model_output.tsv", sep='\t')
-    print(find_metric(df))
+    model_dir = 'experiments/'
+    models = ['base_model', 'majority_model', 'random_model', 'length_model', 'frequency_model']
 
-    # Double check the values obtained
-    df.dropna(subset=["prediction"], inplace=True)
-    print(classification_report(np.array(df.gold), np.array(df.prediction), digits=2))
+    for model in models:
+        df = pd.read_csv(model_dir + model + '/model_output.tsv', sep='\t')
+        print('-----------Metrics for %s-----------' % model)
+        print(find_metric(df))
+
+        # Double check the values obtained
+        df.dropna(subset=["prediction"], inplace=True)
+        print(classification_report(np.array(df.gold), np.array(df.prediction), digits=2))
+
     
